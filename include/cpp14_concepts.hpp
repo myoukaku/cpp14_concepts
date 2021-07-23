c151ce98a951c8f28fd4d7510e52d4bfb75540b7
myoukaku <myoukaku@hotmail.com>
2021-07-23 17:07:58 +0900

    REQUIRED_PARAM マクロと overload_priority クラステンプレートを追加

--- include/cpp14_concepts.hpp
+++ include/cpp14_concepts.hpp
@@ -9,9 +9,24 @@
 #ifndef CPP14_CONCEPTS_HPP_INCLUDED
 #define CPP14_CONCEPTS_HPP_INCLUDED
 
+#define PP_JOIN(X, Y) PP_DO_JOIN(X, Y)
+#define PP_DO_JOIN(X, Y) PP_DO_JOIN2(X,Y)
+#define PP_DO_JOIN2(X, Y) X##Y
+
+#define PP_EXPAND(x) x
+
+#define PP_VA_ARG_COUNT(...) PP_EXPAND(PP_VA_ARG_DO_COUNT(__VA_ARGS__, 9,8,7,6,5,4,3,2,1,0))
+#define PP_VA_ARG_DO_COUNT(_1,_2,_3,_4,_5,_6,_7,_8,_9,N,...) N
+
+#define REQUIRED_PARAM(C, ...)		PP_EXPAND(PP_JOIN(REQUIRED_PARAM_, PP_VA_ARG_COUNT(__VA_ARGS__))(C, __VA_ARGS__))
+
 #if defined(__cpp_concepts) && (__cpp_concepts >= 201907)
 
 #include <concepts>
+
+#define REQUIRED_PARAM_1(C, T)		C T
+#define REQUIRED_PARAM_2(C, T1, T2)	C<T1> T2
+#define REQUIRED_PARAM_3(C, T1, T2, T3)	C<T1, T2> T3
 
 namespace cpp14_concepts
 {
@@ -30,6 +45,10 @@ using std::constructible_from;
 #else
 
 #include <type_traits>
+
+#define REQUIRED_PARAM_1(C, T)		typename T, std::enable_if_t<C<T>>* = nullptr
+#define REQUIRED_PARAM_2(C, T1, T2)	typename T2, std::enable_if_t<C<T2, T1>>* = nullptr
+#define REQUIRED_PARAM_3(C, T1, T2, T3)	typename T3, std::enable_if_t<C<T3, T1, T2>>* = nullptr
 
 namespace cpp14_concepts
 {
@@ -64,5 +83,20 @@ constexpr bool constructible_from =
 }	// namespace cpp14_concepts
 
 #endif
+
+#include <cstddef>
+
+namespace cpp14_concepts
+{
+
+template <std::size_t N>
+struct overload_priority : public overload_priority<N - 1>
+{};
+
+template <>
+struct overload_priority<0>
+{};
+
+}	// namespace cpp14_concepts
 
 #endif // CPP14_CONCEPTS_HPP_INCLUDED
---
+++ test/src/test_required_param.cpp
@@ -0,0 +1,70 @@
+﻿/**
+ *	@file	test_required_param.cpp
+ *
+ *	@brief	REQUIRED_PARAM のテスト
+ *
+ *	@author	myoukaku
+ */
+
+#include <cpp14_concepts.hpp>
+#include <type_traits>
+#include <utility>
+
+namespace required_param_test
+{
+
+template <REQUIRED_PARAM(cpp14_concepts::integral, T)>
+std::integral_constant<int, 1> f1(T);
+
+template <REQUIRED_PARAM(cpp14_concepts::floating_point, T)>
+std::integral_constant<int, 2> f1(T);
+
+static_assert(std::is_same<std::integral_constant<int, 1>, decltype(f1(std::declval<int>()))>::value, "");
+static_assert(std::is_same<std::integral_constant<int, 1>, decltype(f1(std::declval<unsigned int>()))>::value, "");
+static_assert(std::is_same<std::integral_constant<int, 2>, decltype(f1(std::declval<float>()))>::value, "");
+static_assert(std::is_same<std::integral_constant<int, 2>, decltype(f1(std::declval<double>()))>::value, "");
+
+struct A
+{
+	A(int){}
+};
+
+struct B
+{
+	B(char*){}
+};
+
+struct C
+{
+	C(int, int){}
+};
+
+template <REQUIRED_PARAM(cpp14_concepts::constructible_from, int, T)>
+std::integral_constant<int, 1> f2(T);
+
+template <REQUIRED_PARAM(cpp14_concepts::constructible_from, char*, T)>
+std::integral_constant<int, 2> f2(T);
+
+template <REQUIRED_PARAM(cpp14_concepts::constructible_from, int, int, T)>
+std::integral_constant<int, 3> f2(T);
+
+static_assert(std::is_same<std::integral_constant<int, 1>, decltype(f2(std::declval<int>()))>::value, "");
+static_assert(std::is_same<std::integral_constant<int, 1>, decltype(f2(std::declval<A>()))>::value, "");
+static_assert(std::is_same<std::integral_constant<int, 2>, decltype(f2(std::declval<char*>()))>::value, "");
+static_assert(std::is_same<std::integral_constant<int, 2>, decltype(f2(std::declval<B>()))>::value, "");
+static_assert(std::is_same<std::integral_constant<int, 3>, decltype(f2(std::declval<C>()))>::value, "");
+
+template <REQUIRED_PARAM(cpp14_concepts::integral, T)>
+std::integral_constant<int, 1> f3_impl(T, cpp14_concepts::overload_priority<0>);
+
+template <REQUIRED_PARAM(cpp14_concepts::unsigned_integral, T)>
+std::integral_constant<int, 2> f3_impl(T, cpp14_concepts::overload_priority<1>);
+
+template <typename T>
+auto f3(T t) -> decltype(f3_impl(t, cpp14_concepts::overload_priority<1>{}));
+
+
+static_assert(std::is_same<std::integral_constant<int, 1>, decltype(f3(std::declval<int>()))>::value, "");
+static_assert(std::is_same<std::integral_constant<int, 2>, decltype(f3(std::declval<unsigned int>()))>::value, "");
+
+}	// namespace required_param_test
